94d05ca397104386c7cf0adbd0889bde223a9a9b
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-08-11 10:48:36 +0100

    Delete YardGro_Backend directory

--- YardGro_Backend/profiles/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import FarmerProfile, BuyerProfile, RecyclerProfile
-
-admin.site.register(FarmerProfile)
-admin.site.register(BuyerProfile)
-admin.site.register(RecyclerProfile)
-
